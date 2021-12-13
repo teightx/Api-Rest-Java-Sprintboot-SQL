93e89a433f49ece7802a9f261470ed81cc10784c
Thiago Santos <92951789+teightx@users.noreply.github.com>
2021-12-13 15:22:45 -0300

    API
    API Rest utilizando JAVA, Spring boot e consulta em MySQL.
    
    Ferramentas utilizadas:
    IntelliJ CE
    Java Open JDK 14
    Spring Boot
    Banco MySQL
    DbVizualizer
    Postman

---
+++ src/main/java/teight/Thiago Santos/rest/api/TutorialRestApiApplication.java
@@ -0,0 +1,13 @@
+package thiagosantos.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeightRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/expertostech/tutorial/rest/api/TutorialRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package thiagosantos.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeightRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
